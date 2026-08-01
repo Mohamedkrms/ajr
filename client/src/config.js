f2f8d0e4b3c8e8c0779bcdb7b741508ff2c36600
mohamed karmous <MEDKAR928.@Gmail.com>
2026-08-01 17:29:36 +0100

    Refactor API_URL export to handle trailing slashes

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
-export const API_URL = import.meta.env.VITE_API_URL !== undefined && import.meta.env.VITE_API_URL !== ''
-  ? import.meta.env.VITE_API_URL
+const envUrl = import.meta.env.VITE_API_URL;
+export const API_URL = (envUrl !== undefined && envUrl !== '')
+  ? envUrl.replace(/\/+$/, '')
   : (import.meta.env.MODE === 'production' ? '' : 'http://localhost:5000');
+
+
